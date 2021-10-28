07b4468c006631ae1150e629a0eec21f94107395
Administrator <dev@leadingsoft.eu>
2021-10-28 09:54:44 +0300

    commit-03

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDDl8OT17Gf-zBXQ0peeZY5Bz87jwFXa7w",
+    authDomain: "easybank-landing-page-e52e2.firebaseapp.com",
+    projectId: "easybank-landing-page-e52e2",
+    storageBucket: "easybank-landing-page-e52e2.appspot.com",
+    messagingSenderId: "1029637865787",
+    appId: "1:1029637865787:web:097d18df945d349205f71e",
+    measurementId: "G-G6ZTYVDF4R"
+  }
 };
 
 /*
