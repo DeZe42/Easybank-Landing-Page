68f8691deb672b025815c1f20821b5f01a1a37a3
Administrator <dev@leadingsoft.eu>
2021-11-08 10:29:37 +0200

    commit-04

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDDl8OT17Gf-zBXQ0peeZY5Bz87jwFXa7w",
-    authDomain: "easybank-landing-page-e52e2.firebaseapp.com",
-    projectId: "easybank-landing-page-e52e2",
-    storageBucket: "easybank-landing-page-e52e2.appspot.com",
-    messagingSenderId: "1029637865787",
-    appId: "1:1029637865787:web:097d18df945d349205f71e",
-    measurementId: "G-G6ZTYVDF4R"
+    apiKey: "AIzaSyAi0t6KueK0cCbRgMxz0d0F52QZWRmS20o",
+    authDomain: "portfolio-projects-4b9da.firebaseapp.com",
+    projectId: "portfolio-projects-4b9da",
+    storageBucket: "portfolio-projects-4b9da.appspot.com",
+    messagingSenderId: "949907467161",
+    appId: "1:949907467161:web:de282f16c8f42ea4494108",
+    measurementId: "G-VT3ZEHE9ST"
   }
 };
